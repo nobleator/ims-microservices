a14d98a418be62997aa10a4dc3574991500b3039
nobleator <noble.ben.16@gmail.com>
2019-10-21 18:11:18 -0400

    Remove custom comparer and search via .Contains

--- transaction-service/Controllers/TransactionsController.cs
+++ transaction-service/Controllers/TransactionsController.cs
@@ -171,11 +171,9 @@ namespace transaction_service.Controllers
 
             using (var db = new TransactionServiceDbContext())
             {
-                var comparer = new SiteSearchComparer(query);
                 sites = db.Transactions
-                    .Where(t => !t.Deleted)
-                    .OrderBy(t => t.SiteName, comparer)
-                    .Take(10)
+                    .Where(t => !t.Deleted &&
+                                t.SiteName.Contains(query))
                     .Select(t => new DTO.SiteSearchResult
                     {
                         SiteName = t.SiteName,
@@ -189,128 +187,4 @@ namespace transaction_service.Controllers
             return Ok(sites);
         }
     }
-
-    #region Helpers
-    public class SiteSearchComparer : IComparer<string>
-    {
-        private string _query { get; set; }
-        private int _threshold { get; set; }
-        
-        public SiteSearchComparer(string query, int threshhold = 100)
-        {
-            _query = query;
-            _threshold = threshhold;
-        }
-
-        public int Compare(string x, string y)
-        {
-            // Check if the strings are the same
-            if (x == y) return 0;
-            // Check if either string exactly matches the query
-            if (String.Equals(x, _query)) return -1;
-            if (String.Equals(y, _query)) return 1;
-            // Check if either string exactly contains the query
-            // If both contain the query, return the shorter length string
-            if (x.Contains(_query) && y.Contains(_query)) return x.Length - y.Length;
-            if (x.Contains(_query)) return -1;
-            if (y.Contains(_query)) return 1;
-            // Finally, sum edit distances for each word in each string
-            // Note: if this has perfomance issues then use the following instead:
-            // var xEditDist = DamerauLevenshteinDistance(x, _query, _threshold);
-            var xEditDist = x.Split().Sum(word => DamerauLevenshteinDistance(word, _query, _threshold));
-            var yEditDist = y.Split().Sum(word => DamerauLevenshteinDistance(word, _query, _threshold));
-            return xEditDist - yEditDist;
-        }
-
-        // Courtesy of: https://stackoverflow.com/questions/9453731/how-to-calculate-distance-similarity-measure-of-given-2-strings
-        private static int DamerauLevenshteinDistance(string source, string target, int threshold)
-        {
-            int length1 = source.Length;
-            int length2 = target.Length;
-
-            // Return trivial case - difference in string lengths exceeds threshhold
-            if (Math.Abs(length1 - length2) > threshold)
-            {
-                return int.MaxValue;
-            }
-
-            // Ensure arrays [i] / length1 use shorter length 
-            if (length1 > length2)
-            {
-                Swap(ref target, ref source);
-                Swap(ref length1, ref length2);
-            }
-
-            int maxi = length1;
-            int maxj = length2;
-
-            int[] dCurrent = new int[maxi + 1];
-            int[] dMinus1 = new int[maxi + 1];
-            int[] dMinus2 = new int[maxi + 1];
-            int[] dSwap;
-
-            for (int i = 0; i <= maxi; i++)
-            {
-                dCurrent[i] = i;
-            }
-
-            int jm1 = 0, im1 = 0, im2 = -1;
-
-            for (int j = 1; j <= maxj; j++)
-            {
-                // Rotate
-                dSwap = dMinus2;
-                dMinus2 = dMinus1;
-                dMinus1 = dCurrent;
-                dCurrent = dSwap;
-
-                // Initialize
-                int minDistance = int.MaxValue;
-                dCurrent[0] = j;
-                im1 = 0;
-                im2 = -1;
-
-                for (int i = 1; i <= maxi; i++)
-                {
-                    int cost = source[im1] == target[jm1] ? 0 : 1;
-
-                    int del = dCurrent[im1] + 1;
-                    int ins = dMinus1[i] + 1;
-                    int sub = dMinus1[im1] + cost;
-
-                    //Fastest execution for min value of 3 integers
-                    int min = (del > ins) ? (ins > sub ? sub : ins) : (del > sub ? sub : del);
-
-                    if (i > 1 && j > 1 && source[im2] == target[jm1] && source[im1] == target[j - 2])
-                    {
-                        min = Math.Min(min, dMinus2[im2] + cost);
-                    }
-
-                    dCurrent[i] = min;
-                    if (min < minDistance)
-                    {
-                        minDistance = min;
-                    }
-                    im1++;
-                    im2++;
-                }
-                jm1++;
-                if (minDistance > threshold)
-                {
-                    return int.MaxValue;
-                }
-            }
-
-            int result = dCurrent[maxi];
-            return (result > threshold) ? int.MaxValue : result;
-        }
-        
-        private static void Swap<T>(ref T arg1, ref T arg2)
-        {
-            T temp = arg1;
-            arg1 = arg2;
-            arg2 = temp;
-        }
-    }
-    #endregion
 }
