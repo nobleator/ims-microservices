270f63a032d2b28fb131847200ac409774ecec68
nobleator <noble.ben.16@gmail.com>
2019-10-27 12:50:31 -0400

    Add get_candidate_deliveries SQL function

---
+++ db/SQL Scripts/0.0.0.0/011-Create_get_candidate_deliveries.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION get_candidate_deliveries(as_of TIMESTAMP) RETURNS TABLE(line_item_id INT, delivery_priority INT, profit NUMERIC, site_latitude FLOAT, site_longitude FLOAT) AS $$
+    WITH purchases_cte (product_id, avg_purchase_price) AS (
+        SELECT li.product_id, AVG(li.price / li.quantity) AS avg_purchase_price
+        FROM public.line_item li
+        JOIN public.transaction t ON t.transaction_id = li.transaction_id
+        WHERE t.transaction_type = 'Purchase'
+        GROUP BY li.product_id
+    )
+
+    SELECT li.line_item_id,
+        t.priority,
+        (li.price / li.quantity) - pcte.avg_purchase_price AS profit,
+        t.site_latitude,
+        t.site_longitude
+    FROM public.line_item li
+    JOIN public.transaction t ON t.transaction_id = li.transaction_id
+    JOIN purchases_cte pcte ON pcte.product_id = li.product_id
+    WHERE NOT li.deleted
+        AND NOT t.deleted
+        AND t.transaction_type = 'Sale'
+        AND t.deliver_after < (as_of AT TIME ZONE 'utc')
+        AND t.deliver_before > (as_of AT TIME ZONE 'utc')
+        AND t.status <> 'Scheduled'
+    ORDER BY t.priority DESC;
+$$ LANGUAGE SQL;
